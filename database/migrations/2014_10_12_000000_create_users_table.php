a47bd5c03484192311e0516ff52aa22eb2be595e
eccoka <ecco0505@gmail.com>
2024-01-23 18:08:20 +0000

    users table expanded and create_customer_table migration added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username') ->nullable();
+            $table->string('first_name') ->nullable();
+            $table->string('last_name') ->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone') ->nullable();
+            $table->enum('role', ['admin', 'leader', 'agent', 'customer' ])->default('customer');
+            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->string('password');
+            $table->tinyInteger('newsletter');
             $table->rememberToken();
             $table->timestamps();
         });
